d3aa5343500ce6977a8d1f1d928621903de30e6f
Sam Doran <sdoran@redhat.com>
2016-11-17 11:28:36 -0500

    Initial commit

---
+++ scripts/cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Remove Ansible and its dependencies.
+yum -C -y autoremove ansible
+
+# Delete old kernels
+package-cleanup -C -y --oldkernels --count=1
+
+# Clean yum cache
+rm -rf /var/cache/yum/*
+
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
+
