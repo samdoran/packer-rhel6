bc4dea5f6539efe6e4a88f24f5c2a72b3d7cddb3
Sam Doran <sdoran@redhat.com>
2017-03-24 17:21:19 -0400

    Cleanup more things to save space
    Do not remove python-six since it also will remove subscription-manager.
    Cleanup backgrounds and kernel-firmware to save space.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove Ansible and its dependencies.
-yum -C -y remove ansible python-crypto python-paramiko python-markupsafe python-httplib2 sshpass python-setuptools python-crypto2 python-simplejson python-pyasn1 python-keyczar libyaml PyYAML python-six python-babel python-jinja2
+yum -C -y remove kernel-firmware ansible libyaml python-babel python-crypto python-crypto2.6 python-httplib2 python-httplib2 python-jinja2-26 python-keyczar python-markupsafe python-paramiko python-pyasn1 python-setuptools python-simplejson sshpass PyYAML
 
 # Delete old kernels
 package-cleanup -C -y --oldkernels --count=1
@@ -12,6 +12,7 @@ rm -rf /var/cache/yum/*
 rm -rf /usr/share/man/*
 rm -rf /usr/share/info/*
 rm -rf /usr/share/doc/*
+rm -rf /usr/share/backgrounds/*
 
 # Zero out the rest of the free space using dd, then delete the written file.
 echo "Reclaming free space on disk"
